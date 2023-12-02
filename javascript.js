e929c2850618aa763376a5a52ad5404886331661
munirsubhan40 <148547167+munirsubhan40@users.noreply.github.com>
2023-12-02 19:31:22 +0500

    add file

---
+++ javascript.js
@@ -0,0 +1,42 @@
+alert("The page that you're looking for used information that you entered. Returning to that page might cause any action you took to be repeated. Do you want to continue?")
+
+let a="Hello";
+let b="World!";
+
+console.log(a+ " " +b)
+
+console.log("Hey! Welcome to my web");
+/*alert("Hello World 10");
+alert("Hello World 09");
+alert("Hello World 08");
+alert("Hello World 07");
+alert("Hello World 06");
+alert("Hello World 05");
+alert("Hello World 04");
+alert("Hello World 03");
+alert("Hello World 02");
+alert("Hello World 01");
+*/
+//Comment
+
+//variable
+var subhan_age = 19;
+console.log('subhan_age');
+console.log(subhan_age);
+
+//latest variable
+let age= 19;
+console.log(age);
+//const age= 19;  
+
+//boolen : true or false
+let x= 2>3
+console.log(x);
+
+//array
+let x=["subhan","bilal","faris"];
+console.log[x];
+//object
+let man {name ;"subhan", age;"18"};
+console.log(man);
+*/
